a931ac3f2e8cb5306c8fc42cfa2ff5b412c34312
Amit <theamitprajapaati@gmail.com>
2026-04-01 14:33:26 +0530

    deb

--- db.js
+++ db.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose");
 
-let client = mongoose.connect("mongodb://localhost:27017/villager")
+try {
+    let client = mongoose.connect("mongodb://localhost:27017/villager")
 
-client.then(value=>console.log("connect DB")).catch(err=>console.log("ERROR",err))
+    client.then(value => console.log("connect DB")).catch(err => console.log("ERROR", err))
 
-//test d
+    //test d 
+} catch (error) {
+    console.log("ERROR", error)
+}
+
