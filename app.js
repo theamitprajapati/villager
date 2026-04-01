f1086d2b13203200b5322603cb608e4aaadfd4cc
Amit <theamitprajapaati@gmail.com>
2026-04-01 12:55:31 +0530

    add good commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const UAParser = require('ua-parser-js');
+
+const app = express();
+
+app.get('/track', (req, res) => {
+    const userAgent = req.headers['user-agent'];
+    const parser = new UAParser(userAgent);
+    const result = parser.getResult();
+
+    const data = {
+        ip: req.ip,
+        userAgent: userAgent,
+        device: result.device,
+        os: result.os,
+        browser: result.browser
+    };
+
+    console.log("User Info:", data);
+
+    // You can store in DB here
+
+    res.send("Tracking captured ✅");
+});
+
+app.listen(9000, () => {
+    console.log("Server running on port 3000");
+});
